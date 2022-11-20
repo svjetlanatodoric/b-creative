74e49cfefe8631c1acf5b4a2d95aadd38f6d35e3
Svjetlana <svjetlanatodoric03@gmail.com>
2022-11-20 23:02:17 +0100

    Popravljena responzivnost blog sekcije

--- js/script.js
+++ js/script.js
@@ -8,7 +8,9 @@ var logoGallery = document.getElementById("logo-gallery");
 var designGallery = document.getElementById("design-gallery");
 var devGallery = document.getElementById("dev-gallery");
 
-var blogSection = document.getElementById("blog-section")
+var blogSection = document.getElementById("blog-section");
+
+var footerSection = document.getElementById("footer");
 
 
 // LOGO GALLERY
@@ -30,21 +32,24 @@ function logoresize() {
             console.log("Screen width is smaller than 767px")
             counterContainer.style.transform = "translate(0,23vw)";
             counterContainer.style.marginTop = "unset";
-            blogSection.style.transform = "translate(0,10%)"
+            blogSection.style.transform = "translate(0,4%)"
         }
         if (window.matchMedia("(min-width:767px)").matches && window.matchMedia("(max-width:1240px)").matches) {
             console.log("Screen bigger than 767px and smaller than 1024px")
             counterContainer.style.transform = "translate(0,23%)";
             counterContainer.style.marginTop = "unset";
-            blogSection.style.transform = "translate(0,-54%)"
+            blogSection.style.transform = "translate(0,-18%)"
 
+            footerSection.style.top="-8em";
 
         }
         if (window.matchMedia("(min-width:1240px)").matches) {
             console.log("Screen bigger than 1240px ")
             counterContainer.style.transform = "translate(0,5%)";
             counterContainer.style.marginTop = "unset";
-            blogSection.style.transform = "translate(0,-60%)"
+            blogSection.style.transform = "translate(0,-21%)"
+
+            footerSection.style.top="-8em";
 
         }
     })
@@ -53,11 +58,12 @@ function logoresize() {
 button2.addEventListener("click", function () {
     var counterContainer = document.getElementById("counter-container");
 
+
     if (window.matchMedia("(max-width: 767px)").matches) {
         console.log("Screen width is smaller than 767px")
         counterContainer.style.transform = "translate(0,23vw)";
         counterContainer.style.marginTop = "unset";
-        blogSection.style.transform = "translate(0,10%)"
+        blogSection.style.transform = "translate(0,4%)"
 
 
     }
@@ -65,8 +71,9 @@ button2.addEventListener("click", function () {
         console.log("Screen bigger than 767px and smaller than 1024px")
         counterContainer.style.transform = "translate(0,23%)";
         counterContainer.style.marginTop = "unset";
-        blogSection.style.transform = "translate(0,-54%)"
+        blogSection.style.transform = "translate(0,-18%)"
 
+        footerSection.style.top="-8em";
 
 
     }
@@ -74,8 +81,9 @@ button2.addEventListener("click", function () {
         console.log("Screen bigger than 1240px ")
         counterContainer.style.transform = "translate(0,5%)";
         counterContainer.style.marginTop = "unset";
-        blogSection.style.transform = "translate(0,-60%)"
+        blogSection.style.transform = "translate(0,-21%)"
 
+        footerSection.style.top="-8em";
 
     }
 })
@@ -84,6 +92,7 @@ button2.addEventListener("click", function () {
 //ALL IMAGES
 
 window.onload = allImages;
+
 function allImages() {
     logoGallery.style.display = "none";
     designGallery.style.display = "none";
@@ -135,13 +144,16 @@ function designresize() {
             console.log("Screen width is smaller than 767px")
             counterContainer.style.transform = "translate(0,-13%)";
             counterContainer.style.marginTop = "unset";
-            blogSection.style.transform = "translate(0,-54%)"
+            blogSection.style.transform = "translate(0,-3%)"
 
         }
         if (window.matchMedia("(min-width:767px)").matches && window.matchMedia("(max-width:1240px)").matches) {
             console.log("Screen bigger than 767px and smaller than 1024px")
             counterContainer.style.transform = "translate(0,-208%)";
             counterContainer.style.marginTop = "unset";
+            blogSection.style.transform = "translate(0,-17%)"
+
+            footerSection.style.top="-8em";
 
         }
 
@@ -149,6 +161,9 @@ function designresize() {
             console.log("Screen bigger than 1240px ")
             counterContainer.style.transform = "translate(0,-208%)";
             counterContainer.style.marginTop = "unset";
+            blogSection.style.transform = "translate(0,-19%)"
+
+            footerSection.style.top="-4em";
 
         }
     })
@@ -160,23 +175,27 @@ button3.addEventListener("click", function () {
     if (window.matchMedia("(max-width: 767px)").matches) {
         console.log("Screen width is smaller than 767px")
         counterContainer.style.transform = "translate(0,-13%)";
-
         counterContainer.style.marginTop = "unset";
+        blogSection.style.transform = "translate(0,-3%)"
+
 
     }
     if (window.matchMedia("(min-width:767px)").matches && window.matchMedia("(max-width:1240px)").matches) {
         console.log("Screen bigger than 767px and smaller than 1024px")
         counterContainer.style.transform = "translate(0,-208%)";
         counterContainer.style.marginTop = "unset";
-        blogSection.style.transform = "translate(0,-54%)"
+        blogSection.style.transform = "translate(0,-17%)"
 
+        footerSection.style.top="-8em";
 
     }
     if (window.matchMedia("(min-width:1240px)").matches) {
         console.log("Screen bigger than 1240px ")
         counterContainer.style.transform = "translate(0,-208%)";
         counterContainer.style.marginTop = "unset";
-        blogSection.style.transform = "translate(0,-54%)"
+        blogSection.style.transform = "translate(0,-19%)"
+
+        footerSection.style.top="-4em";
 
     }
 
@@ -200,7 +219,7 @@ function devresize() {
             console.log("Screen width is smaller than 767px")
             counterContainer.style.transform = "translate(0,20vw)";
             counterContainer.style.marginTop = "unset";
-            blogSection.style.transform = "translate(0,10%)"
+            blogSection.style.transform = "translate(0,3%)"
 
 
         }
@@ -208,17 +227,32 @@ function devresize() {
             console.log("Screen bigger than 767px and smaller than 1024px")
             counterContainer.style.transform = "translate(0,3%)";
             counterContainer.style.marginTop = "unset";
-            blogSection.style.transform = "translate(0,-77%)"
+            blogSection.style.transform = "translate(0,-23%)"
+
+            footerSection.style.top="-16em";
 
 
         }
-        if (window.matchMedia("(min-width:1024px)").matches) {
-            console.log("Screen bigger than 1240px ")
+        if (window.matchMedia("(min-width:1024px)").matches  && window.matchMedia("(max-width:1240px)").matches) {
+            console.log("Screen bigger than 1024px and smaller than 1500px")
             counterContainer.style.transform = "translate(0,-3%)";
             counterContainer.style.marginTop = "unset";
-            blogSection.style.transform = "translate(0,-75%)"
+            blogSection.style.transform = "translate(0,-25%)"
+    
+            footerSection.style.top="-18em";
 
         }
+        if (window.matchMedia("(min-width:1240px)").matches) {
+            console.log("Screen bigger than 1024px and smaller than 1500px")
+            counterContainer.style.transform = "translate(0,-3%)";
+            counterContainer.style.marginTop = "unset";
+            blogSection.style.transform = "translate(0,-28%)"
+    
+            footerSection.style.top="-13em";
+
+        }
+        
+        
 
     })
 }
@@ -229,7 +263,7 @@ button4.addEventListener("click", function () {
         console.log("Screen width is smaller than 767px")
         counterContainer.style.transform = "translate(0,20vw)";
         counterContainer.style.marginTop = "unset";
-        blogSection.style.transform = "translate(0,10%)"
+        blogSection.style.transform = "translate(0,3%)"
 
 
     }
@@ -237,16 +271,27 @@ button4.addEventListener("click", function () {
         console.log("Screen bigger than 767px and smaller than 1024px")
         counterContainer.style.transform = "translate(0,3%)";
         counterContainer.style.marginTop = "unset";
-        blogSection.style.transform = "translate(0,-77%)"
+        blogSection.style.transform = "translate(0,-23%)"
 
+        footerSection.style.top="-18em";
 
     }
-    if (window.matchMedia("(min-width:1024px)").matches) {
+    if (window.matchMedia("(min-width:1024px)").matches  && window.matchMedia("(max-width:1240px)").matches) {
         console.log("Screen bigger than 1024px and smaller than 1500px")
         counterContainer.style.transform = "translate(0,-3%)";
         counterContainer.style.marginTop = "unset";
-        blogSection.style.transform = "translate(0,-75%)"
+        blogSection.style.transform = "translate(0,-25%)"
 
+        footerSection.style.top="-8em";
+
+    }
+    if (window.matchMedia("(min-width:1240px)").matches) {
+        console.log("Screen bigger than 1024px and smaller than 1500px")
+        counterContainer.style.transform = "translate(0,-3%)";
+        counterContainer.style.marginTop = "unset";
+        blogSection.style.transform = "translate(0,-28%)"
+
+        footerSection.style.top="-13em";
 
     }
 })
